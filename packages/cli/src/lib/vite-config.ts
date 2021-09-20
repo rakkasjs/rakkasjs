4e7f6761473396e6dded8bd15f319db69ee63cd4
cyco130 <cyco130@gmail.com>
2021-09-20 13:10:29 +0300

    chore: Refactor and separate the vite plugin

--- packages/cli/src/lib/vite-config.ts
+++ packages/cli/src/lib/vite-config.ts
@@ -1,10 +1,7 @@
-import reactRefresh from "@vitejs/plugin-react-refresh";
 import path from "path";
-import virtual, { updateVirtualModule } from "vite-plugin-virtual";
-import micromatch from "micromatch";
 import { InlineConfig, normalizePath, SSROptions } from "vite";
 import { FullConfig } from "../..";
-import { makeRouteManifest } from "./make-route-manifest";
+import { rakkasVitePlugin } from "./vite-plugin";
 
 export async function makeViteConfig(
 	config: FullConfig,
@@ -15,68 +12,6 @@ export async function makeViteConfig(
 	const publicDir = normalizePath(path.resolve("public"));
 	const pagesDir = normalizePath(config.pagesDir);
 	const apiDir = normalizePath(config.apiDir);
-
-	const componentExtensions = config.pageExtensions.join("|");
-	const PAGES = `/${pagesDir}/**/(*.)?page.(${componentExtensions})`;
-	const LAYOUTS = `/${pagesDir}/**/(*/)?layout.(${componentExtensions})`;
-
-	const apiExtensions = config.endpointExtensions.join("|");
-	const ENDPOINTS = `/${apiDir}/**/(*.)?endpoint.(${apiExtensions})`;
-	const MIDDLEWARE = `/${apiDir}/**/(*/)?middleware.(${apiExtensions})`;
-
-	const isPage = micromatch.matcher(srcDir + PAGES);
-	const isLayout = micromatch.matcher(srcDir + LAYOUTS);
-
-	const isEndpoint = micromatch.matcher(srcDir + ENDPOINTS);
-	const isMiddleware = micromatch.matcher(srcDir + MIDDLEWARE);
-
-	const virtualModules = virtual({
-		"@rakkasjs/page-routes": await makeRouteManifest({
-			srcDir,
-			routesDir: pagesDir,
-			finalExtensions: componentExtensions,
-			wrapperExt: "layout",
-			leafExt: "page",
-			rootUrl: "/",
-		}),
-
-		"@rakkasjs/page-imports": `
-			const pages = import.meta.glob(${JSON.stringify(PAGES)});
-			const layouts = import.meta.glob(${JSON.stringify(LAYOUTS)});
-			export default Object.assign(pages, layouts);
-		`,
-
-		"@rakkasjs/api-imports": `
-			const endpoints = import.meta.glob(${JSON.stringify(ENDPOINTS)});
-			const middleware = import.meta.glob(${JSON.stringify(MIDDLEWARE)});
-			export default Object.assign(endpoints, middleware);
-		`,
-
-		"@rakkasjs/api-routes": await makeRouteManifest({
-			srcDir,
-			routesDir: apiDir,
-			finalExtensions: apiExtensions,
-			wrapperExt: "middleware",
-			leafExt: "endpoint",
-			rootUrl: config.apiRoot + "/",
-		}),
-
-		"@rakkasjs/process-request": `
-			import apiRoutes from "@rakkasjs/api-routes";
-			import pageRoutes from "@rakkasjs/page-routes";
-
-			import {handleRequest} from "rakkasjs/server";
-
-			export function processRequest(req){
-				return handleRequest(apiRoutes,pageRoutes,req);
-			}`,
-	});
-
-	const indexHtmlPath = path.resolve("src", "index.html");
-	const normalizedIndexHtmlPath = normalizePath(indexHtmlPath);
-
-	let ssr: boolean;
-	let mode: string;
 
 	const result: InlineConfig = {
 		...config.vite,
@@ -117,192 +52,16 @@ export async function makeViteConfig(
 		},
 		plugins: [
 			...(config.vite.plugins || []),
-			virtualModules,
-			{
-				name: "rakkas-resolve",
-				enforce: "pre",
-
-				configResolved(config) {
-					ssr = !!config?.build?.ssr;
-					mode = config.mode;
-				},
-
-				configureServer(server) {
-					server.watcher.addListener("all", async (e: string, fn: string) => {
-						if (
-							(isPage(fn) || isLayout(fn)) &&
-							(e === "add" || e === "unlink")
-						) {
-							updateVirtualModule(
-								virtualModules,
-								"@rakkasjs/page-routes",
-								await makeRouteManifest({
-									srcDir,
-									routesDir: pagesDir,
-									finalExtensions: componentExtensions,
-									wrapperExt: "layout",
-									leafExt: "page",
-									rootUrl: "/",
-								}),
-							);
-						} else if (
-							(isEndpoint(fn) || isMiddleware(fn)) &&
-							(e === "add" || e === "unlink")
-						) {
-							updateVirtualModule(
-								virtualModules,
-								"@rakkasjs/api-routes",
-								await makeRouteManifest({
-									srcDir,
-									routesDir: apiDir,
-									finalExtensions: apiExtensions,
-									wrapperExt: "middleware",
-									leafExt: "endpoint",
-									rootUrl: config.apiRoot + "/",
-								}),
-							);
-						} else if (configDeps.includes(fn) && onConfigChange) {
-							// eslint-disable-next-line no-console
-							console.log("Config file dependency", fn, "changed");
-							onConfigChange();
-						}
-					});
-				},
-
-				buildStart() {
-					this.addWatchFile(srcDir + PAGES);
-					this.addWatchFile(srcDir + LAYOUTS);
-					this.addWatchFile(srcDir + ENDPOINTS);
-					this.addWatchFile(srcDir + MIDDLEWARE);
-
-					if (onConfigChange) {
-						configDeps.forEach((dep) => this.addWatchFile(dep));
-					}
-				},
-
-				async resolveId(id, importer, options) {
-					if (id === indexHtmlPath) {
-						return normalizedIndexHtmlPath;
-					} else if (
-						id === "/__rakkas-start-client.js" &&
-						importer === normalizedIndexHtmlPath
-					) {
-						return id;
-					} else if (id === "@rakkasjs/client-hooks") {
-						const userVersion = await this.resolve(
-							path.resolve(srcDir, "client"),
-							importer,
-							{
-								...options,
-								skipSelf: true,
-							},
-						);
-						return userVersion || id;
-					} else if (id === "@rakkasjs/server-hooks") {
-						const userVersion = await this.resolve(
-							path.resolve(srcDir, "server"),
-							importer,
-							{
-								...options,
-								skipSelf: true,
-							},
-						);
-						return userVersion || id;
-					}
-				},
-
-				async load(id) {
-					if (id === normalizedIndexHtmlPath) {
-						return template;
-					} else if (id === "/__rakkas-start-client.js") {
-						return `
-							import {startClient} from "rakkasjs/client";
-							import routes from "@rakkasjs/page-routes";
-							startClient(routes);
-						`;
-					} else if (
-						id === "@rakkasjs/client-hooks" ||
-						id === "@rakkasjs/server-hooks"
-					) {
-						return "";
-					}
-				},
-
-				async generateBundle() {
-					if (ssr) return;
-
-					// TODO: This is pretty stupid, we should just pass the import manifest to the caller using some other technique
-
-					const modules: Record<string, string[]> = {};
-					for (const id of this.getModuleIds()) {
-						const module = this.getModuleInfo(id);
-						if (!module) continue;
-
-						for (const importer of module.importers) {
-							modules[importer] = modules[importer] || [];
-							modules[importer].push(id);
-						}
-					}
-
-					const pageDeps: Record<string, string[]> = {};
-					for (const [id, deps] of Object.entries(modules)) {
-						if (!isPage(id) && !isLayout(id)) continue;
-
-						const depSet = new Set<string>();
-
-						// eslint-disable-next-line no-inner-declarations
-						function addDeps(deps: string[]) {
-							if (!deps.length) return;
-
-							const newDeps = new Set<string>();
-
-							for (const dep of deps) {
-								const imports = modules[dep];
-								if (!imports) continue;
-
-								imports.forEach((x) => {
-									if (!depSet.has(x)) {
-										depSet.add(x);
-										newDeps.add(x);
-									}
-								});
-							}
-
-							addDeps([...newDeps]);
-						}
-
-						const initialDeps = deps.map((x) => x);
-
-						addDeps(initialDeps);
-
-						pageDeps[normalizePath(path.relative(srcDir, id))] = [
-							...depSet,
-						].map((x) => normalizePath(path.relative(srcDir, x)));
-					}
-
-					await this.emitFile({
-						type: "asset",
-						fileName: "import-manifest.json",
-						source: JSON.stringify(pageDeps),
-					});
-				},
-			},
-			{
-				name: "rakkas-refresh-page",
-				enforce: "pre",
-				async transform(code, id, ssr) {
-					if (ssr || mode === "production") return;
-
-					if (isPage(id) || isLayout(id)) {
-						const idstr = JSON.stringify(id.slice(srcDir.length));
-						// return refreshHeader(idstr) + code + refreshFooter(idstr);
-						return code + refreshFooter(idstr);
-					}
-
-					return code;
-				},
-			},
-			reactRefresh({ exclude: [PAGES, LAYOUTS] }),
+			await rakkasVitePlugin({
+				srcDir,
+				pagesDir,
+				apiDir,
+				pageExtensions: config.pageExtensions,
+				endpointExtensions: config.endpointExtensions,
+				apiRoot: config.apiRoot,
+				configDeps,
+				onConfigChange,
+			}),
 		],
 	};
 
@@ -315,44 +74,3 @@ export async function makeViteConfig(
 
 	return result;
 }
-
-const template = `<!DOCTYPE html>
-<html><!-- rakkas-html-attributes-placeholder -->
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
-		<!-- rakkas-head-placeholder -->
-	</head>
-	<body><!-- rakkas-body-attributes-placeholder -->
-		<div id="rakkas-app"><!-- rakkas-app-placeholder --></div>
-		<script type="module" src="/__rakkas-start-client.js"></script>
-	</body>
-</html>`;
-
-const refreshFooter = (id: string) => `// $RefreshReg$()
-	if (import.meta.hot) {
-		import.meta.hot.accept((m) => {
-			function reload() {
-				if (window.__vite_plugin_react_timeout) {
-					requestAnimationFrame(reload);
-				} else {
-					window.$rakkas$reloader[${id}] && window.$rakkas$reloader[${id}](m);
-				}
-			}
-			console.log("Reloading", ${id});
-			reload();
-		});
-
-		// window.$RefreshReg$ = prevRefreshReg;
-		// window.$RefreshSig$ = prevRefreshSig;
-		// console.log("Uninstalled", import.meta.url, prevRefreshSig);
-		// import.meta.hot.accept();
-		// if (!window.__vite_plugin_react_timeout) {
-		// 	window.__vite_plugin_react_timeout = setTimeout(() => {
-		// 		window.__vite_plugin_react_timeout = 0;
-		// 		RefreshRuntime.performReactRefresh();
-		// 	}, 30);
-		// }
-	}
-`;
---
+++ packages/cli/src/lib/vite-plugin.ts
@@ -0,0 +1,321 @@
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import micromatch from ".pnpm/@types+micromatch@4.0.2/node_modules/@types/micromatch";
+import { PluginOption, normalizePath } from "vite";
+import virtual, { updateVirtualModule } from "vite-plugin-virtual";
+import { makeRouteManifest } from "./make-route-manifest";
+import path from "path";
+
+export interface RakkasVitePluginConfig {
+	srcDir: string;
+	pagesDir: string;
+	apiDir: string;
+	pageExtensions: string[];
+	endpointExtensions: string[];
+	apiRoot: string;
+	configDeps: string[];
+	onConfigChange?: () => void;
+}
+
+export async function rakkasVitePlugin(
+	config: RakkasVitePluginConfig,
+): Promise<PluginOption[]> {
+	const {
+		srcDir,
+		pagesDir,
+		apiDir,
+		pageExtensions,
+		endpointExtensions,
+		apiRoot,
+		configDeps,
+		onConfigChange,
+	} = config;
+
+	const componentExtensions = pageExtensions.join("|");
+	const PAGES = `/${pagesDir}/**/(*.)?page.(${componentExtensions})`;
+	const LAYOUTS = `/${pagesDir}/**/(*/)?layout.(${componentExtensions})`;
+
+	const apiExtensions = endpointExtensions.join("|");
+	const ENDPOINTS = `/${apiDir}/**/(*.)?endpoint.(${apiExtensions})`;
+	const MIDDLEWARE = `/${apiDir}/**/(*/)?middleware.(${apiExtensions})`;
+
+	const isPage = micromatch.matcher(srcDir + PAGES);
+	const isLayout = micromatch.matcher(srcDir + LAYOUTS);
+
+	const isEndpoint = micromatch.matcher(srcDir + ENDPOINTS);
+	const isMiddleware = micromatch.matcher(srcDir + MIDDLEWARE);
+
+	const indexHtmlPath = path.resolve("src", "index.html");
+	const normalizedIndexHtmlPath = normalizePath(indexHtmlPath);
+
+	const virtualModules = virtual({
+		"@rakkasjs/page-routes": await makeRouteManifest({
+			srcDir,
+			routesDir: pagesDir,
+			finalExtensions: componentExtensions,
+			wrapperExt: "layout",
+			leafExt: "page",
+			rootUrl: "/",
+		}),
+
+		"@rakkasjs/page-imports": `
+			const pages = import.meta.glob(${JSON.stringify(PAGES)});
+			const layouts = import.meta.glob(${JSON.stringify(LAYOUTS)});
+			export default Object.assign(pages, layouts);
+		`,
+
+		"@rakkasjs/api-imports": `
+			const endpoints = import.meta.glob(${JSON.stringify(ENDPOINTS)});
+			const middleware = import.meta.glob(${JSON.stringify(MIDDLEWARE)});
+			export default Object.assign(endpoints, middleware);
+		`,
+
+		"@rakkasjs/api-routes": await makeRouteManifest({
+			srcDir,
+			routesDir: apiDir,
+			finalExtensions: apiExtensions,
+			wrapperExt: "middleware",
+			leafExt: "endpoint",
+			rootUrl: apiRoot + "/",
+		}),
+
+		"@rakkasjs/process-request": `
+			import apiRoutes from "@rakkasjs/api-routes";
+			import pageRoutes from "@rakkasjs/page-routes";
+
+			import {handleRequest} from "rakkasjs/server";
+
+			export function processRequest(req){
+				return handleRequest(apiRoutes,pageRoutes,req);
+			}`,
+	});
+
+	let ssr: boolean;
+	let mode: "build" | "serve";
+
+	return [
+		virtualModules,
+		{
+			name: "rakkas-resolve",
+			enforce: "pre",
+
+			configResolved(config) {
+				config.command;
+				ssr = !!config?.build?.ssr;
+				mode = config.command;
+			},
+
+			configureServer(server) {
+				server.watcher.addListener("all", async (e: string, fn: string) => {
+					if ((isPage(fn) || isLayout(fn)) && (e === "add" || e === "unlink")) {
+						updateVirtualModule(
+							virtualModules,
+							"@rakkasjs/page-routes",
+							await makeRouteManifest({
+								srcDir,
+								routesDir: pagesDir,
+								finalExtensions: componentExtensions,
+								wrapperExt: "layout",
+								leafExt: "page",
+								rootUrl: "/",
+							}),
+						);
+					} else if (
+						(isEndpoint(fn) || isMiddleware(fn)) &&
+						(e === "add" || e === "unlink")
+					) {
+						updateVirtualModule(
+							virtualModules,
+							"@rakkasjs/api-routes",
+							await makeRouteManifest({
+								srcDir,
+								routesDir: apiDir,
+								finalExtensions: apiExtensions,
+								wrapperExt: "middleware",
+								leafExt: "endpoint",
+								rootUrl: config.apiRoot + "/",
+							}),
+						);
+					} else if (configDeps.includes(fn) && onConfigChange) {
+						// eslint-disable-next-line no-console
+						console.log("Config file dependency", fn, "changed");
+						onConfigChange();
+					}
+				});
+			},
+
+			buildStart() {
+				if (mode === "build") return;
+
+				this.addWatchFile(srcDir + PAGES);
+				this.addWatchFile(srcDir + LAYOUTS);
+				this.addWatchFile(srcDir + ENDPOINTS);
+				this.addWatchFile(srcDir + MIDDLEWARE);
+
+				if (onConfigChange) {
+					configDeps.forEach((dep) => this.addWatchFile(dep));
+				}
+			},
+
+			async resolveId(id, importer, options) {
+				if (id === indexHtmlPath) {
+					return normalizedIndexHtmlPath;
+				} else if (
+					id === "/__rakkas-start-client.js" &&
+					importer === normalizedIndexHtmlPath
+				) {
+					return id;
+				} else if (id === "@rakkasjs/client-hooks") {
+					const userVersion = await this.resolve(
+						path.resolve(srcDir, "client"),
+						importer,
+						{
+							...options,
+							skipSelf: true,
+						},
+					);
+					return userVersion || id;
+				} else if (id === "@rakkasjs/server-hooks") {
+					const userVersion = await this.resolve(
+						path.resolve(srcDir, "server"),
+						importer,
+						{
+							...options,
+							skipSelf: true,
+						},
+					);
+					return userVersion || id;
+				}
+			},
+
+			async load(id) {
+				if (id === normalizedIndexHtmlPath) {
+					return template;
+				} else if (id === "/__rakkas-start-client.js") {
+					return `
+					import {startClient} from "rakkasjs/client";
+					import routes from "@rakkasjs/page-routes";
+					startClient(routes);
+				`;
+				} else if (
+					id === "@rakkasjs/client-hooks" ||
+					id === "@rakkasjs/server-hooks"
+				) {
+					return "";
+				}
+			},
+
+			async transform(code, id, ssr) {
+				if (ssr || mode === "build") return;
+
+				if (isPage(id) || isLayout(id)) {
+					const idstr = JSON.stringify(id.slice(srcDir.length));
+					// return refreshHeader(idstr) + code + refreshFooter(idstr);
+					return code + refreshFooter(idstr);
+				}
+
+				return code;
+			},
+
+			async generateBundle() {
+				if (ssr) return;
+
+				// TODO: This is pretty stupid, we should just pass the import manifest to the caller using some other technique
+
+				const modules: Record<string, string[]> = {};
+				for (const id of this.getModuleIds()) {
+					const module = this.getModuleInfo(id);
+					if (!module) continue;
+
+					for (const importer of module.importers) {
+						modules[importer] = modules[importer] || [];
+						modules[importer].push(id);
+					}
+				}
+
+				const pageDeps: Record<string, string[]> = {};
+				for (const [id, deps] of Object.entries(modules)) {
+					if (!isPage(id) && !isLayout(id)) continue;
+
+					const depSet = new Set<string>();
+
+					// eslint-disable-next-line no-inner-declarations
+					function addDeps(deps: string[]) {
+						if (!deps.length) return;
+
+						const newDeps = new Set<string>();
+
+						for (const dep of deps) {
+							const imports = modules[dep];
+							if (!imports) continue;
+
+							imports.forEach((x) => {
+								if (!depSet.has(x)) {
+									depSet.add(x);
+									newDeps.add(x);
+								}
+							});
+						}
+
+						addDeps([...newDeps]);
+					}
+
+					const initialDeps = deps.map((x) => x);
+
+					addDeps(initialDeps);
+
+					pageDeps[normalizePath(path.relative(srcDir, id))] = [...depSet].map(
+						(x) => normalizePath(path.relative(srcDir, x)),
+					);
+				}
+
+				await this.emitFile({
+					type: "asset",
+					fileName: "import-manifest.json",
+					source: JSON.stringify(pageDeps),
+				});
+			},
+		},
+		reactRefresh({ exclude: [PAGES, LAYOUTS] }),
+	];
+}
+
+const template = `<!DOCTYPE html>
+<html><!-- rakkas-html-attributes-placeholder -->
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+		<!-- rakkas-head-placeholder -->
+	</head>
+	<body><!-- rakkas-body-attributes-placeholder -->
+		<div id="rakkas-app"><!-- rakkas-app-placeholder --></div>
+		<script type="module" src="/__rakkas-start-client.js"></script>
+	</body>
+</html>`;
+
+const refreshFooter = (id: string) => `// $RefreshReg$()
+	if (import.meta.hot) {
+		import.meta.hot.accept((m) => {
+			function reload() {
+				if (window.__vite_plugin_react_timeout) {
+					requestAnimationFrame(reload);
+				} else {
+					window.$rakkas$reloader[${id}] && window.$rakkas$reloader[${id}](m);
+				}
+			}
+			console.log("Reloading", ${id});
+			reload();
+		});
+
+		// window.$RefreshReg$ = prevRefreshReg;
+		// window.$RefreshSig$ = prevRefreshSig;
+		// console.log("Uninstalled", import.meta.url, prevRefreshSig);
+		// import.meta.hot.accept();
+		// if (!window.__vite_plugin_react_timeout) {
+		// 	window.__vite_plugin_react_timeout = setTimeout(() => {
+		// 		window.__vite_plugin_react_timeout = 0;
+		// 		RefreshRuntime.performReactRefresh();
+		// 	}, 30);
+		// }
+	}
+`;
