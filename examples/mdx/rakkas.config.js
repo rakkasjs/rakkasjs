e2ccaf436cddde5848ec7e664dea04f07cdacc41
cyco130 <cyco130@gmail.com>
2021-10-20 01:00:51 +0300

    fix(example-mdx): Add some packages to optimizeDeps to prevent the SSR bug

--- examples/mdx/rakkas.config.js
+++ examples/mdx/rakkas.config.js
@@ -2,5 +2,8 @@ import mdx from "vite-plugin-mdx";
 
 export default {
 	pageExtensions: ["tsx", "jsx", "mdx"],
-	vite: { plugins: [mdx()] },
+	vite: {
+		plugins: [mdx()],
+		optimizeDeps: { include: ["@mdx-js/mdx", "@mdx-js/react"] },
+	},
 };
