33edc1d8d7e951c0b7157fc6c14b24e2dfee7456
cyco130 <cyco130@gmail.com>
2021-12-01 11:43:36 +0300

    fix: Make apollo examples work again

--- examples/apollo/rakkas.config.ts
+++ examples/apollo/rakkas.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from "@rakkasjs/cli";
 export default defineConfig({
 	vite: {
 		optimizeDeps: { include: ["@apollo/client"] },
+		ssr: {
+			external: ["@rakkasjs/apollo-server"],
+		},
 	},
 });
