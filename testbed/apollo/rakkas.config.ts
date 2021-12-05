be0b7a5eb17ad3a78e2867367758369300152049
cyco130 <cyco130@gmail.com>
2021-12-05 11:06:45 +0300

    chore: Update to vite@2.7.0-beta.10

--- testbed/apollo/rakkas.config.ts
+++ testbed/apollo/rakkas.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@rakkasjs/cli";
 
 export default defineConfig({
 	vite: {
-		optimizeDeps: { include: ["@apollo/client"] },
+		// optimizeDeps: { include: ["@apollo/client"] },
 		ssr: {
 			external: ["@rakkasjs/apollo-server"],
 		},
