628b57f757abba623a8116eb8f40e3061607932f
cyco130 <cyco130@gmail.com>
2022-07-21 09:03:17 +0300

    chore: change Helmet's displayName to Head

--- packages/rakkasjs/src/features/head/lib.ts
+++ packages/rakkasjs/src/features/head/lib.ts
@@ -1,2 +1,7 @@
 export type { HelmetProps as HeadProps } from "react-helmet-async";
-export { Helmet as Head } from "react-helmet-async";
+import { ComponentType } from "react";
+import { Helmet } from "react-helmet-async";
+
+(Helmet as ComponentType).displayName = "Head";
+
+export { Helmet as Head };
