6ff8455bf25a31f5af6aa498325ac2003175cfed
cyco130 <cyco130@gmail.com>
2022-11-07 17:15:03 +0300

    chore: improve useId test

--- testbed/kitchen-sink/src/routes/use-id/_useId.page.tsx
+++ testbed/kitchen-sink/src/routes/use-id/_useId.page.tsx
@@ -1,32 +1,19 @@
-import { ClientSuspense, Head, Page, useQuery } from "rakkasjs";
+import { Page, useQuery } from "rakkasjs";
 import { useId } from "react";
 
 export let uniqueId: string;
 
 const UseIdPage: Page = () => {
 	const unique = useId();
+	const serverResult = useQuery("id", () => unique);
 
 	uniqueId = unique;
 
 	return (
-		<ClientSuspense fallback="Loading...">
-			<Check id={unique} />
-		</ClientSuspense>
+		<p id="useIdTestContainer">
+			{unique === serverResult.data ? "Success" : "Fail"}
+		</p>
 	);
 };
-
-function Check(props: { id: string }) {
-	const { data: ssrId } = useQuery("unique-id", (ctx) =>
-		ctx.fetch("/use-id/rendered-id").then((res) => res.text()),
-	);
-
-	return (
-		<p id="useIdTestContainer">{props.id === ssrId ? "Success" : "Fail"}</p>
-	);
-}
-
-UseIdPage.preload = () => ({
-	head: <Head title="useId test" />,
-});
 
 export default UseIdPage;
