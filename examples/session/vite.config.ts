2fb5f3ecf0906c4bd67387990bc88ba5f9179bbf
cyco130 <cyco130@gmail.com>
2023-03-29 17:46:31 +0300

    chore: drop Node 14/16 support

--- examples/session/vite.config.ts
+++ examples/session/vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig((env) => {
 	return {
-		build: env.ssrBuild ? { target: "node16" } : undefined,
 		plugins: [tsconfigPaths(), rakkas()],
 	};
 });
--- packages/rakkasjs/tsup.config.ts
+++ packages/rakkasjs/tsup.config.ts
@@ -50,7 +50,7 @@ export default defineConfig([
 	},
 	{
 		entry: ["./src/lib/client.ts"],
-		target: "node14",
+		target: "node18",
 		format: ["esm"],
 		platform: "node",
 		shims: false,
