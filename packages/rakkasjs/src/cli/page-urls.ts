cee8f3d91ac825bbf8ab2db52944e70c8419c9ce
Fatih Aygün <cyco130@gmail.com>
2024-01-31 10:47:01 +0000

    feat: second iteration on URL builder (#201)

--- packages/rakkasjs/src/cli/page-urls.ts
+++ packages/rakkasjs/src/cli/page-urls.ts
@@ -41,6 +41,8 @@ export async function generatePageUrlBuilder({
 		}
 	>();
 
+	let hasSearch = false;
+
 	for (const path of pageModules) {
 		const sf = program.getSourceFile("src/routes/" + path);
 
@@ -66,6 +68,10 @@ export async function generatePageUrlBuilder({
 			exports.find((e) => e.escapedName === defaultExportName + "Search")
 				?.declarations?.[0] as any
 		)?.name?.escapedText;
+
+		if (search) {
+			hasSearch = true;
+		}
 
 		const hash = (
 			exports.find((e) => e.escapedName === defaultExportName + "Hash")
@@ -93,9 +99,10 @@ export async function generatePageUrlBuilder({
 	}
 
 	// Disable ESLint for the output
-	let output = `/* eslint-disable */\n`;
+	let output =
+		`/* eslint-disable */\n` + `// This file is generated automatically\n\n`;
 
-	if ([...names.values()].some((n) => n.search)) {
+	if (hasSearch) {
 		if (searchModule) {
 			const [module, symbol] = searchModule.split("::");
 			if (!symbol) {
@@ -124,6 +131,9 @@ export async function generatePageUrlBuilder({
 
 	output += `export const pages = {`;
 
+	let urlEscapeUsed = false;
+	let hashSerializerUsed = false;
+
 	for (const [name, { path: pm, search, hash }] of names) {
 		const params = new Set<string>();
 		let catchAllParam: string | undefined;
@@ -131,6 +141,7 @@ export async function generatePageUrlBuilder({
 		const path =
 			"/" +
 			pm
+				.replace(/\\|--/g, "/")
 				.slice(0, -9)
 				.split("/")
 				.filter((segment) => segment !== "index" && !segment.startsWith("_"))
@@ -164,63 +175,83 @@ export async function generatePageUrlBuilder({
 		let decons = "";
 		let type = "never";
 		if (params.size || search || hash) {
-			decons = params.size ? "{ " + [...params].join(", ") + " }" : "_params";
-			type = "{ " + [...params].map((p) => p + ": string").join(", ") + " }";
-			if (!params.size) {
-				type += ` | undefined`;
-			}
-
-			if (search) {
-				type += `, search: ${search}`;
-			}
+			const deconsBits = [...params];
+			type = "{ " + [...params].map((p) => p + ": string").join(", ");
 
 			if (hash) {
-				type += `, hash: ${hash}`;
+				deconsBits.push("hash");
+				if (params.size) type += ", ";
+				type += `hash: ${hash}`;
 			}
+
+			type += " }";
+
+			if (search) {
+				deconsBits.push("..._search");
+				type += ` & ${search}`;
+			}
+
+			decons = `{ ${deconsBits.join(", ")} }`;
 
 			arg = `${decons}: ${type}`;
 
-			if (catchAllParam) {
-				line = `(${arg}) => _u\`${path}\` + ${catchAllParam}`;
+			if (params.size) {
+				if (catchAllParam) {
+					line = `(${arg}) => _u\`${path}\` + ${catchAllParam}`;
+				} else {
+					line = `(${arg}) => _u\`${path}\``;
+				}
+
+				urlEscapeUsed = true;
 			} else {
-				line = `(${arg}) => _u\`${path}\``;
+				line = `(${arg}) => ${JSON.stringify(path)}`;
 			}
 		} else {
-			line = `() => ${JSON.stringify(path)}`;
+			line = `(${arg}) => ${JSON.stringify(path)}`;
 		}
 
 		output += line;
 
 		if (search) {
-			output += ` + _q(search)`;
+			output += ` + _q(_search)`;
 		}
 
 		if (hash) {
 			output += ` + _h(hash)`;
+			hashSerializerUsed = true;
 		}
 
 		output += ",";
 	}
 
-	const union = [...names.values()]
-		.filter((n) => n.search)
-		.map((n) => n.search)
-		.join(" | ");
-
 	output += "\n};\n";
 
-	output += "\ntype _SearchUnion = " + (union || "any") + ";\n";
+	if (urlEscapeUsed) {
+		output += URL_ESCAPE_SRC;
+	}
 
-	output += UTILS_SRC;
+	if (hasSearch) {
+		const union = [...names.values()]
+			.filter((n) => n.search)
+			.map((n) => n.search)
+			.join(" | ");
+		output += "\ntype _SearchUnion = " + (union || "any") + ";\n";
 
-	if (!searchModule) {
-		output += SEARCH_SRC;
+		if (!searchModule) {
+			output += SEARCH_SERIALIZER_SRC;
+		}
+
+		output += SEARCH_WRAPPER_SRC;
+	}
+
+	if (hashSerializerUsed) {
+		output += HASH_SERIALIZER_SRC;
 	}
 
 	writeFileSync("src/page-urls.ts", output);
 }
 
-const UTILS_SRC = `
+const URL_ESCAPE_SRC = `
 function _u(parts: TemplateStringsArray, ...args: string[]): string {
 	let result = "";
 	for (let i = 0; i < parts.length; i++) {
@@ -232,18 +263,22 @@ function _u(parts: TemplateStringsArray, ...args: string[]): string {
 
 	return result;
 }
+`;
 
+const SEARCH_WRAPPER_SRC = `
 function _q(search: _SearchUnion): string {
 	const result = _s(search);
 	return result ? "?" + result : "";
 }
+`;
 
+const HASH_SERIALIZER_SRC = `
 function _h(hash: any): string {
 	return hash ? "#" + hash : "";
 }
 `;
 
-const SEARCH_SRC = `
+const SEARCH_SERIALIZER_SRC = `
 function _s(search: _SearchUnion): string {
 	return new URLSearchParams(search).toString();
 }
