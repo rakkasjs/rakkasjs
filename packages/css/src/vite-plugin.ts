7ec0613027a960fed9fda8f63f66adb3beef82c9
Fatih Aygün <cyco130@gmail.com>
2024-02-24 03:43:24 +0000

    fix: mark @rakkasjs/css as ssr.noExternal

--- packages/css/src/vite-plugin.ts
+++ packages/css/src/vite-plugin.ts
@@ -4,6 +4,9 @@ import type {} from "rakkasjs/vite-plugin";
 export function rakkasCss(): Plugin {
 	return {
 		name: "@rakkasjs/css",
+		config() {
+			return { ssr: { noExternal: ["@rakkasjs/css"] } };
+		},
 		api: {
 			rakkas: {
 				serverHooks: "@rakkasjs/css/server-hooks",
