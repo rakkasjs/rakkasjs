8bd6c6195b584c7ef6a9a807d0741a42dccdae97
Fatih Aygün <cyco130@gmail.com>
2024-03-10 03:59:10 +0000

    chore: create failing test

--- testbed/kitchen-sink/src/routes/head/_head.page.tsx
+++ testbed/kitchen-sink/src/routes/head/_head.page.tsx
@@ -1,20 +1,20 @@
-import { Link, type Page } from "rakkasjs";
+import { Link, useHead, type Page } from "rakkasjs";
 
-const HeadPage: Page = () => (
-	<div>
-		<p>Check my head tags!</p>
-		<Link href="/head/elsewhere">Go elsewhere</Link>
-	</div>
-);
-
-export default HeadPage;
-
-HeadPage.preload = () => ({
-	head: {
+const HeadPage: Page = () => {
+	useHead({
 		title: "The head page",
 		canonical: "http://localhost:3000/head",
 		htmlAttributes: {
 			class: "head-page",
 		},
-	},
-});
+	});
+
+	return (
+		<div>
+			<p>Check my head tags!</p>
+			<Link href="/head/elsewhere">Go elsewhere</Link>
+		</div>
+	);
+};
+
+export default HeadPage;
--- testbed/kitchen-sink/src/routes/head/elsewhere.page.tsx
+++ testbed/kitchen-sink/src/routes/head/elsewhere.page.tsx
@@ -1,10 +1,14 @@
-import { Link, type Page } from "rakkasjs";
+import { Link, useHead, type Page } from "rakkasjs";
 
-const HeadPage2: Page = () => (
-	<div>
-		<p>Check my head tags!</p>
-		<Link href="/head">Go to head test</Link>
-	</div>
-);
+const HeadPage2: Page = () => {
+	useHead({ title: "Head page 2" });
+
+	return (
+		<div>
+			<p>Check my head tags!</p>
+			<Link href="/head">Go to head test</Link>
+		</div>
+	);
+};
 
 export default HeadPage2;
